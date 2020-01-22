000352d9bbb93faec72e1f0e85dbb4ac5d0f6da3
Greg Fisher <gnfisher@thoughtbot.com>
2020-01-21 20:11:59 -0500

    Complete

--- src/MightBe.elm
+++ src/MightBe.elm
@@ -1,10 +1,9 @@
 module MightBe exposing
     ( MightBe(..)
-    , andMap
-    , andThen
     , map
-    , map2
     )
+
+import Debug
 
 
 type MightBe a
@@ -14,41 +13,4 @@ type MightBe a
 
 map : (a -> b) -> MightBe a -> MightBe b
 map fn mightBe =
-    case mightBe of
-        Some val ->
-            Some (fn val)
-
-        None ->
-            None
-
-
-andThen : (a -> MightBe b) -> MightBe a -> MightBe b
-andThen fn mba =
-    case mba of
-        None ->
-            None
-
-        Some val ->
-            fn val
-
-
-andMap : MightBe a -> MightBe (a -> b) -> MightBe b
-andMap mba mbfn =
-    case mba of
-        None ->
-            None
-
-        Some valA ->
-            case mbfn of
-                None ->
-                    None
-
-                Some valFn ->
-                    Some (valFn valA)
-
-
-map2 : (a -> b -> c) -> MightBe a -> MightBe b -> MightBe c
-map2 fn mba mbb =
-    Some fn
-        |> andMap mba
-        |> andMap mbb
+    Debug.todo "Make this test pass by implementing map"
--- tests/MightBeTest.elm
+++ tests/MightBeTest.elm
@@ -2,101 +2,33 @@ module MightBeTest exposing (suite)
 
 import Debug
 import Expect
-import MightBe exposing (MightBe(..), andMap, andThen, map, map2)
+import MightBe exposing (MightBe(..), map)
 import Test exposing (Test, describe, test)
 
 
 suite : Test
 suite =
-    describe "MightBe"
-        [ describe "MightBe is a Functor!"
-            [ test "Must preserve identity morphisms" <|
+    describe "MightBe is a Functor, so it must have a map function"
+        [ describe "A 'Lawful' map must do two things"
+            [ test "1. If you map identity, you get back what you put in" <|
                 \_ ->
                     Some 1
                         |> MightBe.map identity
                         |> Expect.equal (Some 1)
-            , test "preserve composition of morphims" <|
+            , test "2. Mapping a function over a Functor that contains a value, is the same as creating a Functor with the result of applying that function to that value" <|
                 \_ ->
+                    MightBe.map addOne (Some 1)
+                        |> Expect.equal (Some (addOne 1))
+            , test "3. Mapping a composed function over a Functor, is the same as mapping the functions sequentially" <|
+                \_ ->
+                    let
+                        composedFunctions =
+                            double << addOne
+                    in
                     Some 2
                         |> MightBe.map addOne
                         |> MightBe.map double
-                        |> Expect.equal (Some 2 |> MightBe.map (double << addOne))
-            ]
-        , describe "MightBe.map2"
-            [ test "applies function to combine two MightBes" <|
-                \_ ->
-                    MightBe.map2 (\a b -> a + b) (Some 1) (Some 2)
-                        |> Expect.equal (Some 3)
-            , test "If one is None it returns None" <|
-                \_ ->
-                    MightBe.map2 (\a b -> a + b) (Some 1) None
-                        |> Expect.equal None
-            ]
-
-        -- Some 3 |> andThen (Some << double) == (Some << double 3)
-        -- Some 3 |> andThen Some == Some 3
-        , describe "MightBe.andThen aka Monadic Laws"
-            [ test "Right Identity" <|
-                \_ ->
-                    Some 1
-                        |> MightBe.andThen mightDouble
-                        |> Expect.equal (mightDouble 1)
-            , test "Left Identity" <|
-                \_ ->
-                    Some 1
-                        |> MightBe.andThen Some
-                        |> Expect.equal (Some 1)
-            , test "Associativity" <|
-                --(m >>= f) >>= g = m >>= (\x -> f x >>= g)
-                -- m = Some 1
-                -- f = mightDouble
-                -- g = mightAddOne
-                \_ ->
-                    Some 1
-                        |> andThen mightDouble
-                        |> andThen mightAddOne
-                        |> Expect.equal
-                            (Some 1
-                                |> MightBe.andThen
-                                    (\x ->
-                                        mightDouble x
-                                            |> MightBe.andThen mightAddOne
-                                    )
-                            )
-            ]
-        , describe "MightBe.andMap aka Applicative laws "
-            [ test "Identity" <|
-                \_ ->
-                    Some identity
-                        |> MightBe.andMap (Some 1)
-                        |> Expect.equal (Some 1)
-            , test "Homomorphism" <|
-                \_ ->
-                    Some double
-                        |> MightBe.andMap (Some 1)
-                        |> Expect.equal (Some (double 1))
-            , test "Interchange" <|
-                -- u <*> pure y = pure ($ y) <*> u
-                -- u = (Some double)
-                -- y = 1
-                \_ ->
-                    Some double
-                        |> andMap (Some 1)
-                        |> Expect.equal (Some (\fn -> fn 1) |> andMap (Some double))
-            , test "Composition" <|
-                --pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
-                -- u = (Some double)
-                -- v = (Some addOne)
-                -- w = (Some 1)
-                \_ ->
-                    Some (<<)
-                        |> MightBe.andMap (Some double)
-                        |> MightBe.andMap (Some addOne)
-                        |> MightBe.andMap (Some 1)
-                        |> Expect.equal
-                            (Some double
-                                |> MightBe.andMap (Some addOne |> MightBe.andMap (Some 1))
-                            )
+                        |> Expect.equal (Some 2 |> MightBe.map composedFunctions)
             ]
         ]
 
@@ -109,13 +41,3 @@ addOne a =
 double : Int -> Int
 double a =
     a * 2
-
-
-mightDouble : Int -> MightBe Int
-mightDouble n =
-    Some (double n)
-
-
-mightAddOne : Int -> MightBe Int
-mightAddOne n =
-    Some (addOne n)
